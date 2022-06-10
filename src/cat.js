c588e7a3685b82f6b1f1002bd6ae282cd6b1f52d
anna-left <anna_left@mail.ru>
2022-06-10 19:58:54 +0300

    feat: red file

--- src/cat.js
+++ src/cat.js
@@ -1,19 +1,25 @@
 import fs from 'fs';
+import { EOL } from 'os';
+
 import { getPathFromFile } from "./getPathFromFile.js";
+import { showMessageOperationFailed } from "./globalValues.js";
 
-export const cat = async (fileName) => {
+export const cat = (fileName) => {
     fileName = getPathFromFile(fileName);
-    fs.readFile(fileName, 'utf8', (err, data) => {
-        if (err) {
-            console.log('Operation failed');
-        } else {
-            console.log('Content of the file:');
-            console.log('--------------------');
-            console.log(data)
-        };
-    });
-};
 
-// export const cat = async (fileName) => {
-//     await readFile(fileName);
-// }
+    const readStream = fs.createReadStream(fileName);
+
+    return new Promise(function (resolve, reject) {
+        readStream.on('data', chunk => { process.stdout.write(chunk) });
+        readStream.on('end', () => {
+            console.log(EOL);
+            resolve();
+        });
+        readStream.on('error', () => {
+            showMessageOperationFailed();
+            resolve();
+        });
+
+    });
+
+};
